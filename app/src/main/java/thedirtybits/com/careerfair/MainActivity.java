55295996d327f53b7fdd70cafd6578c31e2fff13
Matthew Zilkie <matthewzilkie@Matthews-MacBook-Pro.local>
2017-11-03 23:38:25 -0500

    First commit unfucked up

---
+++ app/src/main/java/thedirtybits/com/careerfair/MainActivity.java
@@ -0,0 +1,13 @@
+package thedirtybits.com.careerfair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
